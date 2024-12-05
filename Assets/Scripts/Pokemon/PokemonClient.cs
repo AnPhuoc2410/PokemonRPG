78d672e1b05c03e3a8b71b3e1306e91f312b54ce
Dao An Phuoc <anphuocdao2410@gmail.com>
2024-12-05 12:47:41 +0700

    Poke Script Obj

---
+++ Assets/Scripts/Pokemon/PokemonClient.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PokemonClient", menuName = "Pokemon/Create new Pokemon")]
+
+public class PokemonClient : ScriptableObject
+{
+    [SerializeField] string pokemonName;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    [SerializeField] int maxHP;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spDefense;
+    [SerializeField] int speed;
+}
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon,
+    Dark,
+    Steel,
+    Fairy
+}
+
